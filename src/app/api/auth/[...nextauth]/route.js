25412b129a6e1183732e10a7386f29a3b3e0648b
heeeete <mkoui@naver.com>
2024-06-10 15:53:04 +0900

    chore : mongoDB DB name 수정

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth';
 import KakaoProvider from 'next-auth/providers/kakao';
 import GoogleProvider from 'next-auth/providers/google';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import { connectDB } from '@/lib/mongodb';
 
 const handler = NextAuth({
   providers: [
@@ -13,6 +15,10 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
+  adapter: MongoDBAdapter(connectDB, {
+    databaseName: 'keynut',
+  }),
 });
 
 export { handler as GET, handler as POST };
