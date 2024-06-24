eadc518d4dee13ce5f31e1f2931f7ae98dc57d3b
licakim <beutilica00@naver.com>
2024-06-24 16:25:11 +0900

    Feat -> 찜 페이지 데이터 가져오기 및 삭제 구현

--- src/app/api/bookmark/route.js
+++ src/app/api/bookmark/route.js
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import { connectDB } from '@/lib/mongodb';
-import getUserSession from '@/app/utils/getUserSession';
+import getUserSession from '@/lib/getUserSession';
 import { ObjectId } from 'mongodb';
 
 const client = await connectDB;
@@ -10,10 +10,16 @@ const users = db.collection('users');
 
 export async function GET(req) {
   try {
-    const ids = await req.json();
-    const objectIds = ids.map(id => new ObjectId(id));
-    const products = await collection.find({ _id: { $in: objectIds } }).toArray();
-    return NextResponse.json(products, { status: 200 });
+    const session = await getUserSession();
+    const { bookmarked } = await users.findOne({ _id: new ObjectId(session.user.id) });
+    console.log('--------------', bookmarked.length);
+    if (!bookmarked || bookmarked.length === 0) {
+      return NextResponse.json([], { status: 200 });
+    }
+    const bookmarkedProducts = await collection
+      .find({ _id: { $in: bookmarked.map(id => new ObjectId(id)) } })
+      .toArray();
+    return NextResponse.json(bookmarkedProducts, { status: 200 });
   } catch (error) {
     console.error('Failed to retrieve bookmarked products:', error);
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
