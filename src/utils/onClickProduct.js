2cba850efaab30bf94b53553274b116dcfbd30da
licakim <beutilica00@naver.com>
2024-07-22 19:55:26 +0900

    Delete -> onClickProduct 삭제

--- src/utils/onClickProduct.js
+++
@@ -1,8 +0,0 @@
-const onClickProduct = e => {
-  const target = e.currentTarget;
-  target.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
-  setTimeout(() => {
-    target.style.backgroundColor = 'transparent';
-  }, 50);
-};
-export default onClickProduct;
