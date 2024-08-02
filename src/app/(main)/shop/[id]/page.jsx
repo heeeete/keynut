198c338a3fc7c6115da63936d59f8cf1511c1a7c
heeeete <mkoui@naver.com>
2024-08-02 22:34:07 +0900

    fix : openGraph metadata 수정
    데이터가 없을때 이미지 안 나오는거 수정

--- src/app/(main)/shop/[id]/page.jsx
+++ src/app/(main)/shop/[id]/page.jsx
@@ -10,9 +10,10 @@ export async function generateMetadata({ params }) {
     title: data ? `${data.nickname}ㅣKEYNUT` : 'KEYNUT',
     openGraph: {
       title: data ? `${data.nickname}ㅣKEYNUT` : 'KEYNUT',
+      description: data ? `${data.nickname}님의 상점` : '상점을 찾을 수 없습니다.',
       images: [
         {
-          url: data ? data.image : '/keynut',
+          url: data ? data.image : `${process.env.NEXT_PUBLIC_BASE_URL}/keynut.png`,
           width: 400,
           height: 400,
         },
