50fd2287fab9a32bd15381a0368e8961201e3fd1
heeeete <mkoui@naver.com>
2024-08-19 16:37:40 +0900

    fix : 메타데이터 수정

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -3,7 +3,11 @@ import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query
 import getRecentProducts from './_lib/getRecentProducts';
 
 export const metadata = {
+  applicationName: 'KEYNUT',
   title: 'KEYNUT - 전자기기 중고거래',
+  description: '커스텀 키보드 | 마우스 | 마우스패드 | 헤드셋 | 모니터는 키넛에서 - 전자기기 중고거래',
+  keywords: ['전자기기', '중고거래', '커스텀 키보드', '키보드', '마우스', '마우스패드', '헤드셋', '모니터', 'KEYNUT'],
+  colorScheme: 'light',
   robots: 'nosnippet',
 };
 
