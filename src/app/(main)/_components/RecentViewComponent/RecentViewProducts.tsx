8ab4c55a76e735454326b90629d3755910797723
licakim <beutilica00@naver.com>
2024-11-29 21:26:30 +0900

    feat: rightbar(최근본상품,top버튼), fix: 상품개수 수정

---
+++ src/app/(main)/_components/RecentViewComponent/RecentViewProducts.tsx
@@ -0,0 +1,92 @@
+'use client';
+import { Fragment, useContext, useEffect, useState } from 'react';
+import Image from 'next/image';
+import { RecentViewContext } from './RecentViewContext';
+import Link from 'next/link';
+
+const deleteViewProduct = (id, setRecentView) => {
+  const prev = JSON.parse(localStorage.getItem('recentView') || '[]');
+
+  const updated = prev.filter(data => data._id !== id);
+
+  localStorage.setItem('recentView', JSON.stringify(updated));
+  setRecentView(updated);
+};
+
+const RecentViewImage = ({ images }) => {
+  return (
+    <div className="w-16 aspect-square relative">
+      <Image
+        className="object-cover"
+        src={images.length ? `${process.env.NEXT_PUBLIC_IMAGE_DOMAIN}/${images[0].name}` : '/noImage.svg'}
+        alt="이미지"
+        fill
+        sizes="200px"
+      />
+    </div>
+  );
+};
+
+const RecentViewTitleAndPrice = ({ product, setRecentView }) => {
+  return (
+    <div className="hidden group-hover:flex flex-col justify-center text-sm absolute right-16 top-0 w-32 h-full border border-r-0 bg-white p-2">
+      <p className="break-all line-clamp-1">{product.title}</p>
+      <div className="space-x-1 break-all line-clamp-1 font-semibold">
+        <span className="">{product.price.toLocaleString()}</span>
+        <span className="text-sm">원</span>
+      </div>
+      <button
+        className="absolute bg-black -top-1 -left-1"
+        onClick={e => {
+          e.preventDefault();
+          e.stopPropagation();
+          deleteViewProduct(product._id, setRecentView);
+        }}
+      >
+        <Image src="/close.svg" alt="deleteRecentView" width={15} height={15} />
+      </button>
+    </div>
+  );
+};
+
+const RecentViewProduct = ({ product, setRecentView }) => {
+  return (
+    <Link className="relative group" key={product._id} href={`/shop/product/${product._id}`}>
+      <RecentViewImage images={product.images} />
+      <RecentViewTitleAndPrice product={product} setRecentView={setRecentView} />
+    </Link>
+  );
+};
+
+const RecentViewProducts = () => {
+  const { recentViewChange, setRecentViewChange } = useContext(RecentViewContext);
+  const [recentView, setRecentView] = useState([]);
+
+  useEffect(() => {
+    let cur = localStorage.getItem('recentView');
+    if (cur && cur.length) setRecentView(JSON.parse(cur));
+    else setRecentView([]);
+    setRecentViewChange(false);
+  }, [recentViewChange]);
+
+  return (
+    <div className="flex flex-col w-24 min-h-30 p-2 rounded-md border bg-white border-gray-300 items-center max-[960px]:hidden">
+      <p className="px-2 pb-1 mb-2 text-xs border-b text-gray-700 font-semibold">최근 본 상품</p>
+      <div className="flex flex-col space-y-3">
+        {recentView && recentView.length ? (
+          recentView.map(product => (
+            <Fragment key={product._id}>
+              <RecentViewProduct product={product} setRecentView={setRecentView} />
+            </Fragment>
+          ))
+        ) : (
+          <div className="p-5">
+            <Image src="/emptyRecentView.svg" alt="emptyView" height={30} width={30} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default RecentViewProducts;
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -3,12 +3,18 @@ import BottomNav from './_components/BottomNav/BottomNav';
 import Nav from './_components/Nav';
 import { ModalProvider } from './_components/ModalProvider';
 import Modal from './_components/Modal';
+import RecentlyViewedProducts from './_components/RightBar';
+import { RecentViewProvider } from './_components/RecentViewComponent/RecentViewContext';
+import RightBar from './_components/RightBar';
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <ModalProvider>
       <Nav />
-      <div className="relative main-1280 max-[960px]:pt-0">{children}</div>
+      <RecentViewProvider>
+        <div className="relative main-1280 max-[960px]:pt-0">{children}</div>
+        <RightBar />
+      </RecentViewProvider>
       <Footer />
       <BottomNav />
       <Modal />
